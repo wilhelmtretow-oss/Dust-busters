c7347adef2e0020b2c7fa4b3fe64927d094ce40f
Sakire09 <sakarias2009@gmail.com>
2026-02-17 10:21:19 +0100

    stuff

--- Assets/Scripts/ContractManager.cs
+++ Assets/Scripts/ContractManager.cs
@@ -8,20 +8,36 @@ public class ContractManager : MonoBehaviour
 
     public string[] possibleTitles = { "Small Home", "Basic Apartment", "Large House" };
     public string[] possibleDiff = { "Easy", "Medium", "Hard" };
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         RandomizeContracts();
     }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     public void RandomizeContracts()
     {
         for (int i = 0; i < titleTexts.Length; i++)
         {
             int randomTitleIndex = Random.Range(0, possibleTitles.Length);
             int randomDiffIndex = Random.Range(0, possibleDiff.Length);
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
             titleTexts[i].text = possibleTitles[randomTitleIndex];
             diffTexts[i].text = possibleDiff[randomDiffIndex];
         }
     }
+<<<<<<< Updated upstream
 }
+=======
+
+}
+>>>>>>> Stashed changes
