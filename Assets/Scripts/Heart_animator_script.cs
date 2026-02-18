1595f993b486a8d0bd307094e7904c17472e7ec0
wilhelmtretow-oss <wilhelm.tretow@elev.ga.lbs.se>
2026-02-18 10:19:21 +0100

    hearts

---
+++ Assets/Scripts/Heart_animator_script.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartsAnimator : MonoBehaviour
+{
+    public Image[] hearts;
+    public Health healthScript;
+    private int nrOfHearts;
+
+    void Start()
+    {
+        nrOfHearts = hearts.Length;
+    }
+
+    void Update()
+    {
+        float frame = (float)healthScript.health / (float)healthScript.maxHealth;
+        DisplayHearts(Mathf.RoundToInt(frame * nrOfHearts));
+    }
+
+    void DisplayHearts(int count)
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            hearts[i].enabled = i < count ? true : false;
+        }
+    }
+}
