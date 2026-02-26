03749215ff9a8c9ff61a9af66dda572f0cf910c8
wilhelmtretow-oss <wilhelm.tretow@elev.ga.lbs.se>
2026-02-26 10:05:49 +0100

    början av shoting script inte klar

--- Assets/Scripts/Projectile_script.cs
+++ Assets/Scripts/Projectile_script.cs
@@ -1,50 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 [RequireComponent(typeof(Rigidbody2D))]
-
-public class Bullet : MonoBehaviour
+public class DirtBall : MonoBehaviour
 {
-    public float bulletSpeed;
-    public int damage;
-    public float lifeTime = 3f;
-    public float knockBack = 0f;
+    public float bulletSpeed = 10f;      // hastighet p� projektilen
+    public int damage = 20;              // skada
+    public float lifeTime = 3f;          // hur l�nge den lever
+    public float knockBack = 0f;         // knockback-effekt
+
+    private Rigidbody2D rb;
 
     void Start()
     {
-        GetComponent<Rigidbody2D>().AddForce(transform.right * bulletSpeed, ForceMode2D.Impulse);
+        rb = GetComponent<Rigidbody2D>();
+
+        // R�kna ut riktning mot musen
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePos - transform.position).normalized;
+
+        // Skjut projektilen
+        rb.AddForce(direction * bulletSpeed, ForceMode2D.Impulse);
+
+        // F�rst�ra projektilen efter lifeTime
         Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+        // Snurra projektilen f�r "damm-effekt"
+        transform.Rotate(0, 0, 360 * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // return if bullet is colliding with player
+        // Ignore Player
         if (collision.gameObject.CompareTag("Player"))
             return;
 
-        // Try get Health component from colliding object
+        // H�mta Health-komponent
         Health health = collision.gameObject.GetComponent<Health>();
 
-        // Hits object with Health component
-        if (health)
+        if (health != null)
         {
+            // Ge skada
             health.TakeDamage(damage);
+
+            // Knockback
             if (knockBack > 0f)
             {
-                // Try get colliding objects rigid body and add knock back.
-                Rigidbody2D rb = collision.gameObject.GetComponentInParent<Rigidbody2D>();
-                if (rb != null)
-                    rb.AddForce(GetComponent<Rigidbody2D>().linearVelocity.normalized * knockBack);
+                Rigidbody2D enemyRb = collision.gameObject.GetComponent<Rigidbody2D>();
+                if (enemyRb != null)
+                    enemyRb.AddForce(rb.linearVelocity.normalized * knockBack, ForceMode2D.Impulse);
             }
         }
-        // Hits other object
-        else
-        {
-            // Do something
-        }
 
+        // F�rst�ra projektilen vid kollision
         Destroy(gameObject);
     }
 }
