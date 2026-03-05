6f095a5235713da61e7fae753df3250e5f9790e6
Sakire09 <sakarias2009@gmail.com>
2026-03-05 09:48:23 +0100

    HelpButton stuff

--- Assets/Sakarias/HelpAdvance.cs
+++ Assets/Sakarias/HelpAdvance.cs
@@ -5,29 +5,29 @@ public class HelpAdvance : MonoBehaviour
 {
     public TMP_Text helpText;       // Drag your TextMeshPro UI here
     public string[] dialogueLines;  // Put your dialogue lines here
+
+    public TMP_Text currentText;
     private int currentLine = 0;
 
     void Start()
     {
         if (dialogueLines.Length > 0)
         {
-            helpText.text = dialogueLines[0]; // Show first line
+            UpdateUI();
         }
     }
 
     // Call this from a Button's OnClick
     public void NextLine()
     {
-        currentLine++;
+        currentLine = (currentLine + 1) % dialogueLines.Length;
 
-        if (currentLine < dialogueLines.Length)
-        {
-            helpText.text = dialogueLines[currentLine]; // Replace text
-        }
-        else
-        {
-            helpText.text = dialogueLines[0];
-            currentLine = 0;
-        }
+        UpdateUI();
+    }
+    public void UpdateUI()
+    {
+        helpText.text = dialogueLines[currentLine];
+
+        currentText.text = (currentLine + 1) + " / " + dialogueLines.Length;
     }
 }
