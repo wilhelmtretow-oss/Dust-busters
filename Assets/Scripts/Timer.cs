0ce9b3f9ce2a3ecdcee74d4e0df4b03ab6f80fe4
Tattoo182 <armin.karajica@gmail.com>
2026-03-19 10:06:17 +0100

    alright

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+ContractData.SelectedDifficulty;
+public class Timer : MonoBehaviour
+{
+    public float time = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
